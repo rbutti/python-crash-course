044d0201c8d1b2e57d0f50e1a8e081d5be3d61cc
Ravikiran Butti <ravikiran.butti@metrobank.plc.uk>
2020-07-12 20:31:23 +0100

    Chapter18

--- com/rbutti/pythoncrashcourse/chapter19/learning_log/learning_log/settings.py
+++ com/rbutti/pythoncrashcourse/chapter19/learning_log/learning_log/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'learning_logs',
-
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'users:login'
